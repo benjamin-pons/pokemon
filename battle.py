06c0536261088a563996fd7e93335b1b4b01292e
Anthony Rahajarison <anthony.rahajrison@laplateforme.io>
2025-02-14 10:54:24 +0100

    feat : added save menu and battle screen

--- battle.py
+++ battle.py
@@ -4,8 +4,32 @@ import json
 import random
 import time
 
+# Load pokemon list file
 with open("pokemonList.json", "r") as json_file :
     pokemonList = json.load(json_file)
+
+SCREEN_WIDTH = 1100
+SCREEN_HEIGHT = 800
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Pokemon")
+
+background_game = pygame.image.load(r"./assets/images/background/background_game.jpg")
+background_game = pygame.transform.scale(background_game, (1100, 800))
+
+# In combat UI
+button_attack = pygame.image.load(r"./assets/images/buttons/button_attack.png")
+button_attack = pygame.transform.scale(button_attack, (200, 200))
+rect_button_attack = button_attack.get_rect(topleft=(890, 600))
+
+button_run = pygame.image.load(r"./assets/images/buttons/button_run.png")
+button_run = pygame.transform.scale(button_run, (200, 200))
+rect_button_run = button_run.get_rect(topleft=(720, 600))
+
+def display_battle():
+    screen.blit(background_game,(0,0))
+    screen.blit(button_run,rect_button_run)
+    screen.blit(button_attack,rect_button_attack)
+    pygame.display.update()
 
 # Input pokedex index to load corresponding pokemon
 def loadPokemon(number) :
@@ -19,10 +43,6 @@ def loadPokemon(number) :
     type2 = pokemonList[number]["type2"]
     return Pokemon(name, base_hp, atk, defense, lvl, type1, type2)
 
-pokemon_ally = loadPokemon(1)
-pokemon_ennemy = loadPokemon(4)
-
-
 
 #Attack
 def action_attack(target) :
@@ -34,13 +54,11 @@ def start_battle() :
 
     battle = True
     while battle :
-        for event in pygame.event :
+        for event in pygame.event.get() :
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if rect_button_attack.collidepoint(event.pos) :
                     action_attack(pokemon_ennemy)
-                elif rect_button_change_pokemon.collidepoint(event.pos) :
-                    action
-                elif rect_button_escape.collidepoint(event.pos) :
-                    action
-    
-
+                elif rect_button_run.collidepoint(event.pos) :
+                    battle = False
+        
+        display_battle()
--- menu_pokemon.py
+++ menu_pokemon.py
@@ -1,5 +1,6 @@
 import pygame
 from battle import start_battle
+import battle
 
 pygame.init()
 
@@ -17,6 +18,9 @@ background_pokedex = pygame.transform.scale(background_pokedex, (1100, 800))
 background_game = pygame.image.load(r"./assets/images/background/background_game.jpg")
 background_game = pygame.transform.scale(background_game, (1100, 800))
 
+background_save = pygame.image.load(r"./assets/images/background/background_save.jpg")
+background_save = pygame.transform.scale(background_save, (1100, 800))
+
 logo = pygame.image.load(r"./assets/images/logo.png")
 logo = pygame.transform.scale(logo, (400, 150))
 
@@ -32,11 +36,15 @@ rect_button_play = button_play.get_rect(topleft=(-80, 100))
 
 button_quit = pygame.image.load(r"./assets/images/buttons/button_quit.png")
 button_quit = pygame.transform.scale(button_quit, (800, 400))
-rect_button_quit = button_quit.get_rect(topleft=(160, 400))
+rect_button_quit = button_quit.get_rect(topleft=(400, 400))
 
 button_pokedex = pygame.image.load(r"./assets/images/buttons/button_pokedex.png")
 button_pokedex = pygame.transform.scale(button_pokedex, (800, 400))
 rect_button_pokedex = button_pokedex.get_rect(topleft=(400, 100))  
+
+button_save = pygame.image.load(r"./assets/images/buttons/save_button.png")
+button_save = pygame.transform.scale(button_save, (800, 400))
+rect_button_save = button_save.get_rect(topleft=(-80, 400))
 
 button_back = pygame.image.load(r"./assets/images/buttons/button_back.png")
 button_back = pygame.transform.scale(button_back, (80, 80))
@@ -48,6 +56,7 @@ def display_main_menu():
     screen.blit(logo,(350,80))
     screen.blit(button_play, rect_button_play.topleft)
     screen.blit(button_quit, rect_button_quit.topleft)
+    screen.blit(button_save,rect_button_save)
     screen.blit(button_pokedex, rect_button_pokedex.topleft)
     pygame.display.update()
 
@@ -56,8 +65,8 @@ def display_pokedex():
     screen.blit(button_back,rect_button_back)
     pygame.display.update()
 
-def display_game():
-    screen.blit(background_game,(0,0))
+def display_save():
+    screen.blit(background_save, (0, 0))
     screen.blit(button_back,rect_button_back)
     pygame.display.update()
 
@@ -73,11 +82,15 @@ while running:
             if current_screen == "menu":
                 if rect_button_play.collidepoint(event.pos):
                     current_screen = "game"
+                    battle.start_battle()
+                    current_screen = "menu"
                 if rect_button_pokedex.collidepoint(event.pos):
                     current_screen = "pokedex"
                 if rect_button_quit.collidepoint(event.pos):
                     running = False
-            elif current_screen in ["game", "pokedex"]:
+                if rect_button_save.collidepoint(event.pos):
+                    current_screen = "save"
+            elif current_screen in ["game", "pokedex", "save"]:
                 if rect_button_back.collidepoint(event.pos):
                     current_screen = "menu"
 
@@ -90,6 +103,7 @@ while running:
         display_pokedex()
     elif current_screen == "game":
         pygame.mixer.music.stop()
-        display_game()
+    elif current_screen == "save":
+        display_save()
 
 pygame.quit()
